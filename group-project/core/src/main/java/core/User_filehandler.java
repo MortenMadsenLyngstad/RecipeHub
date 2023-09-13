5bf4cf9ec50a80317e1b0e9d5e671d855f20bb53
Morten M Lyngstad <Morteml@stud.ntnu.no>
2023-09-13 18:04:41 +0200

    Added file User_filehandler.java

---
+++ group-project/core/src/main/java/core/User_filehandler.java
@@ -0,0 +1,22 @@
+package core;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class User_filehandler {
+
+
+  //   public static void writeUserinfo() {
+  //   try (
+  //       FileWriter filewriter = new FileWriter("group-project/core/src/main/java/textfiles/userinfo.txt", true);
+  //       BufferedWriter bufferedWriter = new BufferedWriter(filewriter);
+  //       PrintWriter output = new PrintWriter(bufferedWriter)) {
+  //       output.println("Username: " + Profile.getUsername() + ", Password: " + Profile.getPassword());
+  //   } catch (IOException e) {
+  //     System.out.println("Error writing to file");
+  //     System.out.println(e.getStackTrace());
+  //   }
+  // }
+}
