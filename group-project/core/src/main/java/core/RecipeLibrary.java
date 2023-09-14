0899b4efec1b771c7f76dab1991ec6071a17eee9
Adrian Solberg <adrianhsolberg@gmail.com>
2023-09-14 17:59:21 +0200

    Made basic logic for RecipeLibrary class
    Made a RecipeLibrary class which keeps contol over seeveral recipes
    
    #14

---
+++ group-project/core/src/main/java/core/RecipeLibrary.java
@@ -0,0 +1,82 @@
+package core;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * This class is used to contain several recipes
+ * @author Adrian Haabpiht Solberg
+ */
+public class RecipeLibrary implements Iterable<Recipe> {
+    private ArrayList<Recipe> recipes;
+
+    /**
+     * This contructor initilizes an empty ArrayList
+     */
+    public RecipeLibrary() {
+        recipes = new ArrayList<>();
+    }
+
+    /**
+     * This contructor initilizes a new ArrayList using the ArrayList sent in
+     * @param recipes - Arraylist filled with recipes
+     */
+    public RecipeLibrary(ArrayList<Recipe> recipes) {
+        this.recipes = new ArrayList<>(recipes);
+    }
+
+    /**
+     * This metod returns the recipe with the given index
+     * @param n - The index of the given recipe
+     * @throws IllegalArgumentException if the index is invalid
+     * @return The recipe with index n in recipes
+     */
+    public Recipe getRecipe(int n) {
+        if (n < 0 || n > getSize()) {
+            throw new IllegalArgumentException("The index is invalid");
+        }
+        return recipes.get(n);
+    }
+
+    /**
+     * This method returns the amount of recipes
+     * @return Integer value with the amount of recipes
+     */
+    public int getSize() {
+        return recipes.size();
+    }
+
+    /**
+     * This method will add the recipe to the ArrayList recipes
+     * @param recipe - Recipe object you wish to add
+     */
+    public void addRecipe(Recipe recipe) {
+        if (!recipes.contains(recipe)) {
+            recipes.add(recipe);
+        }
+    }
+
+    /**
+     * This method will remove thee given recipe from the ArrayList recipes
+     * @param recipe - Recipe object you wish to remove
+     * @throws IllegalArgumentException if the recipe given does not exist in recipes
+     */
+    public void removeRecipe(Recipe recipe) {
+        if (!recipes.remove(recipe))
+            throw new IllegalArgumentException("Recipe not found in recipes");
+    }
+
+    /**
+     * This method defines an iterator for the class, which makes it iterable
+     * @return Iterator which iterates over the elements in recipes
+     */
+    @Override
+    public Iterator<Recipe> iterator() {
+        return recipes.iterator();
+    }
+
+
+
+
+    
+}
