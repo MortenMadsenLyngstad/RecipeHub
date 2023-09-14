41a0830deefeb4cdc1c9a13940c922bb020050f3
Morten M Lyngstad <Morteml@stud.ntnu.no>
2023-09-14 15:51:10 +0200

    Changed LoginController.java
    Added javadocs to improve readabilty

--- group-project/ui/src/main/java/ui/LoginController.java
+++ group-project/ui/src/main/java/ui/LoginController.java
@@ -10,6 +10,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
+
 public class LoginController {
     private User_filehandler user_filehandler = new User_filehandler();
     private SwitchController SwitchController = new SwitchController();
@@ -23,6 +24,14 @@ public class LoginController {
     @FXML
     private PasswordField passwordField;
 
+    /**
+     * Validates the login information
+     * @param uname
+     * @param pword
+     * @return true if the login information is correct, false otherwise
+     * @throws Exception if the user_filehandler.getUserinfo() method throws an exception
+     * @see User_filehandler#getUserinfo()
+     */
     private boolean validateLogin(String uname, String pword) throws Exception {
         if (user_filehandler.getUserinfo().get(uname) == null) {
             loginMessageLabel.setText("Incorrect username or password");
@@ -35,6 +44,13 @@ public class LoginController {
         }
     }
 
+    /**
+     * Logs the user in if the login information is correct
+     * @param event
+     * @throws Exception if the validateLogin method throws an exception
+     * @throws IOException if the SwitchController.switchToMainScreen method throws an exception
+     * @see SwitchController#switchSceneMain(ActionEvent, String)
+     */
     public void login(ActionEvent event) throws Exception {
         if (usernameField.getText().isBlank() == false && passwordField.getText().isBlank() == false) {
             if (validateLogin(usernameField.getText(), passwordField.getText())) {
@@ -45,6 +61,11 @@ public class LoginController {
         }
     }
 
+    /**
+     * Switches to the register screen
+     * @param event
+     * @throws IOException if the SwitchController.switchSceneMain method throws an exception
+     */
     public void switchToRegisterScreen(ActionEvent event) throws IOException {
         System.out.println(user_filehandler.getUserinfo());
 
