0ce2f7534a0cf2381faf5665755b550623efa51c
Morten Madsen Lyngstad <morteml@stud.ntnu.no>
2023-09-15 00:21:42 +0200

    Changes to App.java
    Changed syntax to get UserLogin.fxml-file in start-method

--- group-project/ui/src/main/java/ui/App.java
+++ group-project/ui/src/main/java/ui/App.java
@@ -7,12 +7,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import ui.controllers.LoginController;
 
 public class App extends Application{
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("UserLogin.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginController.class.getResource("UserLogin.fxml"));
         Parent parent = fxmlLoader.load();
         stage.setScene(new Scene(parent));
         stage.show();
