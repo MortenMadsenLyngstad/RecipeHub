235748efb5923f0b7ce960c5b45237b61847b867
olabbi <olabbi@ntnu.no>
2023-09-15 15:51:07 +0200

    change in filestructure in ui, created AddRecipeController and changes in addRecipe.fxml

---
+++ group-project/ui/src/main/java/ui/controllers/AddRecipeController.java
@@ -0,0 +1,61 @@
+package ui.controllers;
+
+import java.io.IOException;
+
+import core.Recipe;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class AddRecipeController {
+    private Recipe newRecipe;
+    private SwitchController switchController = new SwitchController();
+
+    @FXML
+    private Button BackButton;
+
+    @FXML
+    private TextField RecipeName;
+
+    @FXML
+    private Button AddName;
+
+    @FXML
+    private Text NameText;
+
+    @FXML
+    private Text IngredientsText;
+
+    @FXML private TextField IngredientNameInput;
+    
+
+    /**
+     * Logs the user in if the login information is correct
+     * 
+     * @param event
+     * @throws Exception   if the validateLogin method throws an exception
+     * @throws IOException if the SwitchController.switchToMainScreen method throws
+     *                     an exception
+     * @see SwitchController#switchSceneMain(ActionEvent, String)
+     */
+    public void backButtonClick(ActionEvent event) throws IOException {
+        switchController.switchSceneMain(event, "Mainscreen.fxml");
+    }
+
+    private void createNewRecipe(String newRecipeName) {
+        newRecipe = new Recipe(newRecipeName, 1);
+    }
+
+    public void validateNewRecipeName() throws IOException {
+        if (!RecipeName.getText().isBlank()) {
+            createNewRecipe(RecipeName.getText());
+            AddName.setVisible(false);
+            NameText.setVisible(false);
+            IngredientsText.setVisible(true);
+
+        }
+    }
+
+}
