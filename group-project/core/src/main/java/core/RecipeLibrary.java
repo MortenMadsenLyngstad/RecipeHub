db37e7f5c3e9c66b60efe3e4dabb64bbb91ae06d
Adrian Solberg <adrianhsolberg@gmail.com>
2023-09-17 19:18:52 +0200

    Added extra validation in RecipeLibrary
    Added extra validation to the addRecipe(Recipe) method in RecipeLibrary.
    Made it throw exception for duplicates and null.

--- group-project/core/src/main/java/core/RecipeLibrary.java
+++ group-project/core/src/main/java/core/RecipeLibrary.java
@@ -51,9 +51,13 @@ public class RecipeLibrary implements Iterable<Recipe> {
      * @param recipe - Recipe object you wish to add
      */
     public void addRecipe(Recipe recipe) {
-        if (!recipes.contains(recipe)) {
-            recipes.add(recipe);
+        if (recipes.contains(recipe)) {
+            throw new IllegalArgumentException("The recipe is already added");
         }
+        if (recipe == null) {
+            throw new IllegalArgumentException("Can't add null");
+        }
+        recipes.add(recipe);
     }
 
     /**
