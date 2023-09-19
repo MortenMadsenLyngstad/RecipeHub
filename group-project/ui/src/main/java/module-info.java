a6600fecb5fa27204d2b7429f43f6fab9296e411
Morten M Lyngstad <Morteml@stud.ntnu.no>
2023-09-19 12:06:37 +0200

    Fixed bugs when running mvn test
    Changed placement of jacocoargline in pom-file
    Changed module-info from ui to ui.controllers and added javafx.base
    
    Issue: #29

--- group-project/ui/src/main/java/module-info.java
+++ group-project/ui/src/main/java/module-info.java
@@ -1,9 +1,10 @@
-module ui {
+module ui.controllers {
     requires core;
     requires file;
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.base;
 
     opens ui.controllers to javafx.fxml, javafx.graphics;
-    opens ui to javafx.graphics, javafx.fxml;
+    opens ui to javafx.fxml, javafx.graphics;
 }
