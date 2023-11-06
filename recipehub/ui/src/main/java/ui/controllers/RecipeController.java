9d8452fdd680c3acb8f3524ce22f7f5eee0287df
olabbi <olabbi@ntnu.no>
2023-11-06 09:47:30 +0100

    Adapted RecipeController to the new changes in Recipe.fxml
    Issue: #86

--- recipehub/ui/src/main/java/ui/controllers/RecipeController.java
+++ recipehub/ui/src/main/java/ui/controllers/RecipeController.java
@@ -12,11 +12,11 @@ import java.util.Optional;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
-import javafx.scene.control.DialogPane;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
@@ -25,6 +25,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import org.controlsfx.control.Rating;
@@ -38,7 +39,6 @@ public class RecipeController extends SuperController {
     private Boolean flag = false;
     private Alert alert;
     private Alert ratingAlert;
-    private Alert commentsAlert;
     private double scale = 1.0;
     private int portions = 0;
 
@@ -58,6 +58,11 @@ public class RecipeController extends SuperController {
     private Text averageRating;
     @FXML
     private Hyperlink numberOfRaters, numberOfComments;
+    @FXML
+    private VBox scrollPaneBox;
+    @FXML
+    private ScrollPane scrollPane;
+    
 
     /**
      * Handels the click of the backButton, sends the user back to the mainscreen.
@@ -76,6 +81,7 @@ public class RecipeController extends SuperController {
         // If the recipe is the users own, show the delete button
         showDeleteButton();
         showRating();
+        showComments();
 
         mainscreenController.setHeart(heartButton, recipe, currentProfile);
         nameField.setText(recipe.getName());
@@ -132,7 +138,6 @@ public class RecipeController extends SuperController {
      * Clears the text from stepsText and ingredientsText to avoid dupliucates.
      * Intended use with scale operations.
      */
-
     private void clearText() {
         ingredientsText.setText("");
         stepsText.setText("");
@@ -219,6 +224,61 @@ public class RecipeController extends SuperController {
         }
     }
 
+    private void showComments() {
+        if (recipe.getNumberOfcomments() > 0) {
+            if (scrollPaneBox.getChildren().size() > 1) {
+                scrollPaneBox.getChildren().remove(1);
+                scrollPaneBox.getChildren().remove(1);
+            }
+            Label reviewLabel = new Label("Reviews:");
+            reviewLabel.setFont(new javafx.scene.text.Font("System", 24));
+            scrollPaneBox.getChildren().add(1, reviewLabel);
+
+            //Makes the splitpane for the comments
+            SplitPane commentsPane = new SplitPane();
+            commentsPane.setPrefWidth(462);
+            commentsPane.setOrientation(Orientation.VERTICAL);
+            
+            //Sorts the list of comments so the newest is on top
+            List<Review> reviews = new ArrayList<>(recipe.getReviews().stream()
+                    .filter(r -> !r.getComment().equals("")).toList());
+            Collections.reverse(reviews);
+
+            //Makes a vbox for each comment
+            for (Review r : reviews) {
+                if (!r.getComment().equals("")) {
+                    VBox vbox2 = new VBox();
+
+                    //Adds the username, rating and comment to the vbox
+                    vbox2.getChildren().add(new Label(r.getReviewer() + ": "));
+                    Rating rating = new Rating();
+                    rating.setRating(1);
+                    rating.setMax(1);
+                    rating.setDisable(true);
+                    vbox2.getChildren().add(rating);
+                    vbox2.getChildren()
+                            .add(new Label(String.valueOf(r.getRating()).charAt(0) + "/5"));
+                    vbox2.setPrefWidth(140);
+
+                    //Adds the vbox to a hbox with the comment
+                    HBox hbox = new HBox();
+                    hbox.getChildren().add(vbox2);
+                    HBox.setHgrow(vbox2, Priority.ALWAYS);
+                    vbox2.setAlignment(Pos.CENTER);
+                    TextArea textArea = new TextArea(r.getComment());
+                    textArea.setWrapText(true);
+                    textArea.setPrefHeight(40);
+                    textArea.setPrefWidth(350);
+                    textArea.setEditable(false);
+                    hbox.getChildren().add(textArea);
+                    hbox.setDisable(true);
+                    commentsPane.getItems().add(hbox);
+                }
+            }
+            scrollPaneBox.getChildren().add(2, commentsPane);
+        } 
+    }
+
     /**
      * Handels the click of the deleteButton.
      * 
@@ -232,7 +292,7 @@ public class RecipeController extends SuperController {
         if (showAlert()) {
             ActionEvent actionEvent = new ActionEvent(event.getSource(), event.getTarget());
             try {
-                switchSceneWithInfo(actionEvent, "Mainscreen.fxml");
+                switchSceneMain(actionEvent, "Mainscreen.fxml");
             } catch (IOException e1) {
                 e1.printStackTrace();
             }
@@ -335,52 +395,17 @@ public class RecipeController extends SuperController {
         recipe.addReview(new Review(rating, comment, currentProfile.getUsername()));
         recipeFilehandler.writeRecipe(this.recipe);
         showRating();
+        showComments();
     }
     
     /**
      * Shows a popup with the comments on the recipe.
      * The comments are shown in a scrollable list.
      */
-    public void showCommentsPopup() {
-        commentsAlert = new Alert(AlertType.INFORMATION);
-        commentsAlert.setTitle("Comments on this recipe");
-        commentsAlert.setDialogPane(new DialogPane());
-        commentsAlert.getDialogPane().setPrefHeight(300);
-        commentsAlert.getDialogPane().setPrefWidth(450);
-        
-        ButtonType closeButton = ButtonType.CLOSE;
-        commentsAlert.getButtonTypes().add(closeButton);
-        
-        List<Review> reviews = new ArrayList<>(recipe.getReviews().stream()
-                .filter(r -> !r.getComment().equals("")).toList());
-        Collections.reverse(reviews);
-        
-        VBox vbox = new VBox();
-        for (Review r : reviews) {
-            HBox hbox = new HBox();
-            hbox.getChildren().add(new Label(r.getReviewer() + ": "));
-            TextArea textArea = new TextArea(r.getComment());
-            textArea.setWrapText(true);
-            textArea.setPrefHeight(40);
-            textArea.setPrefWidth(350);
-            textArea.setEditable(false);
-            hbox.getChildren().add(textArea);
-            hbox.setDisable(true);
-            vbox.getChildren().add(hbox);
-        }
-        
-        ScrollPane scrollPane = new ScrollPane();
-        scrollPane.setContent(vbox);
-        scrollPane.setFitToWidth(true);
-        
-        commentsAlert.getDialogPane().setContent(scrollPane);
-        
-        Optional<ButtonType> result = commentsAlert.showAndWait();
-        if (result.isPresent() && result.get() == closeButton) {
-            commentsAlert.close();
-        }
-        
-        this.commentsAlert = null;
+    public void scrollDownToComments() {
+        Double height = scrollPaneBox.getHeight();
+        Double position = stepsLabel.getLayoutY();
+        scrollPane.setVvalue(1 - ((position + 240) / height));
     }
     
     /**
@@ -390,14 +415,5 @@ public class RecipeController extends SuperController {
      */
     public Alert getRatingAlert() {
         return this.ratingAlert;
-    }
-
-    /**
-     * Returns the current commentsAlert.
-     * 
-     * @return The commentsAlert
-     */
-    public Alert getCommentsAlert() {
-        return this.commentsAlert;
     }
 }
