6c222c910765c7fd9a1247e495081f5e3427bd3f
Adrian Solberg <adrianhsolberg@gmail.com>
2023-11-14 19:15:23 +0100

    Small indentation fix in PasswordHasherTest.java

--- recipehub/core/src/test/java/core/PasswordHasherTest.java
+++ recipehub/core/src/test/java/core/PasswordHasherTest.java
@@ -21,7 +21,7 @@ public class PasswordHasherTest {
         Assertions.assertNotEquals("Password123", hashedPassword, "The password should be hashed");
         String[] hashedPasswordArray = hashedPassword.split(":");
         Assertions.assertEquals(2, hashedPasswordArray.length,
-                "The hashed password should be split into two parts");
+            "The hashed password should be split into two parts");
     }
 
     /**
@@ -36,8 +36,8 @@ public class PasswordHasherTest {
         String hashedPassword = PasswordHasher.hashPassword("Password123");
         String wrongHashedPassword = hashedPassword + ":wrong";
         Assertions.assertFalse(PasswordHasher.verifyPassword("Password123", wrongHashedPassword),
-                "The password should not be verified");
+            "The password should not be verified");
         Assertions.assertTrue(PasswordHasher.verifyPassword("Password123", hashedPassword),
-                "The password should be verified");
+            "The password should be verified");
     }
 }
